b172730681a16dc549a6404a8098928245849ed9
jblanco081 <116687716+jblanco081@users.noreply.github.com>
2024-07-06 15:20:49 -0500

    Add files via upload

---
+++ utils/jwt.js
@@ -0,0 +1,11 @@
+const jwt = require('jsonwebtoken');
+
+function generateToken(user) {
+    const payload = {
+        id: user._id,
+        username: user.username
+    };
+    return jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '1h' });
+}
+
+module.exports = generateToken;
